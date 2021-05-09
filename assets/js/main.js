747f41332d788c5829b41e26e730e7ad623e8ef1
Claudie Larouche <claudielarouche@gmail.com>
2021-05-09 07:30:41 -0400

    Update main.js
    remove lightbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,18 +161,18 @@
   /**
    * Initiate portfolio lightbox 
    */
-  const portfolioLightbox = GLightbox({
+  /*const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
-  });
+  });*/
 
   /**
    * Initiate portfolio details lightbox 
    */
-  const portfolioDetailsLightbox = GLightbox({
+  /*const portfolioDetailsLightbox = GLightbox({
     selector: '.portfolio-details-lightbox',
     width: '90%',
     height: '90vh'
-  });
+  });*/
 
   /**
    * Portfolio details slider
