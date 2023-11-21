1d292b1d4bd3f3b96b4c4138061ed86f4d916695
Claudie Larouche <claudielarouche@gmail.com>
2023-11-20 20:04:58 -0500

    date

--- earlyON/earlyon-dev.js
+++ earlyON/earlyon-dev.js
@@ -1,71 +1,72 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let originalData = []; // Initialize as an empty array
 
-    // Update the path to your CSV file
-    const csvFilePath = 'https://claudielarouche.com/earlyON/data.csv';
+        let originalData = []; // Initialize as an empty array
 
-    Papa.parse(csvFilePath, {
-        header: true,
-        download: true,
-        skipEmptyLines: true,
-        complete: function(results) {
-            if (results.errors.length > 0) {
-                console.error('Error parsing CSV:', results.errors);
-                document.getElementById('csvData').innerHTML = 'Error loading data.';
-            } else if (Array.isArray(results.data)) {
-                originalData = results.data;
-                renderTable(originalData);
-            } else {
+        window.onload = function () {
+            // Update the path to your CSV file
+            const csvFilePath = 'https://claudielarouche.com/earlyON/data.csv';
+
+            Papa.parse(csvFilePath, {
+                header: true,
+                download: true,
+                skipEmptyLines: true,
+                complete: function(results) {
+                    if (results.errors.length > 0) {
+                        console.error('Error parsing CSV:', results.errors);
+                        document.getElementById('csvData').innerHTML = 'Error loading data.';
+                    } else if (Array.isArray(results.data)) {
+                        originalData = results.data;
+                        renderTable(originalData);
+                    } else {
+                        console.error('Error loading data: Data is not an array.');
+                        document.getElementById('csvData').innerHTML = 'Error loading data.';
+                    }
+                },
+                error: function(error) {
+                    console.error('Error fetching or parsing CSV:', error);
+                    document.getElementById('csvData').innerHTML = 'Error loading data.';
+                }
+            });
+        };
+
+        function renderTable(data) {
+            // Ensure data is an array
+            if (!Array.isArray(data)) {
                 console.error('Error loading data: Data is not an array.');
                 document.getElementById('csvData').innerHTML = 'Error loading data.';
+                return;
             }
-        },
-        error: function(error) {
-            console.error('Error fetching or parsing CSV:', error);
-            document.getElementById('csvData').innerHTML = 'Error loading data.';
-        }
-    });
 
-    function renderTable(data) {
-        // Ensure data is an array
-        if (!Array.isArray(data)) {
-            console.error('Error loading data: Data is not an array.');
-            document.getElementById('csvData').innerHTML = 'Error loading data.';
-            return;
-        }
+            // Check if data is empty
+            if (data.length === 0) {
+                console.warn('No data available.');
+                document.getElementById('csvData').innerHTML = 'No data available.';
+                return;
+            }
 
-        // Check if data is empty
-        if (data.length === 0) {
-            console.warn('No data available.');
-            document.getElementById('csvData').innerHTML = 'No data available.';
-            return;
-        }
+            const headers = Object.keys(data[0]);
 
-        const headers = Object.keys(data[0]);
+            let tableHtml = '<table id="dataTable"><thead><tr>';
+            headers.forEach(header => {
+                tableHtml += `<th>${header}</th>`;
+            });
+            tableHtml += '</tr></thead><tbody>';
 
-        let tableHtml = '<table id="dataTable"><thead><tr>';
-        headers.forEach(header => {
-            tableHtml += `<th>${header}</th>`;
-        });
-        tableHtml += '</tr></thead><tbody>';
+            const selectedDate = document.getElementById('selectedDate').value;
 
-        const selectedDate = document.getElementById('selectedDate').value;
+            const filteredData = filterDataByDate(data, selectedDate);
 
-        const filteredData = filterDataByDate(data, selectedDate);
-
-        if (!Array.isArray(filteredData)) {
-            console.error('Error loading data: Filtered data is not an array.');
-            document.getElementById('csvData').innerHTML = 'Error loading data.';
-            return;
-        }
-
-        if (filteredData.length === 0) {
+            if (!Array.isArray(filteredData)) {
+                console.error('Error loading data: Filtered data is not an array.');
+                document.getElementById('csvData').innerHTML = 'Error loading data.';
+                return;
+            }
+			
+			if (filteredData.length === 0) {
             document.getElementById('csvData').innerHTML = 'No playgroups found for the selected day.';
             return;
         }
 
-
-		filteredData.forEach(row => {
+            filteredData.forEach(row => {
                 const currentDate = row['Date'] ? row['Date'] : '';
                 if (!isPastDate(currentDate)) {
                     tableHtml += '<tr>';
@@ -81,32 +82,35 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             });
 
-        
+            tableHtml += '</tbody></table>';
 
-        tableHtml += '</tbody></table>';
-
-        document.getElementById('csvData').innerHTML = tableHtml;
-    }
-
-    function filterDataByDate(data, selectedDate) {
-        // If no date is selected, return the original data
-        if (!selectedDate) {
-            return data;
+            document.getElementById('csvData').innerHTML = tableHtml;
         }
 
-        // Ensure data is an array before filtering
-        if (Array.isArray(data)) {
-            return data.filter(row => {
-                const currentDate = row['Date'] ? row['Date'] : '';
-                return currentDate === selectedDate;
-            });
-        } else {
-            return [];
-        }
-    }
+        function filterDataByDate(data, selectedDate) {
+            // If no date is selected, return the original data
+            if (!selectedDate) {
+                return data;
+            }
 
-    // Listen for changes in date input
-    document.getElementById('selectedDate').addEventListener('change', function() {
-        renderTable(originalData);
-    });
-});
+            // Ensure data is an array before filtering
+            if (Array.isArray(data)) {
+                return data.filter(row => {
+                    const currentDate = row['Date'] ? row['Date'] : '';
+                    return currentDate === selectedDate;
+                });
+            } else {
+                return [];
+            }
+        }
+
+        function isPastDate(dateString) {
+            const currentDate = new Date();
+            const selectedDate = new Date(dateString);
+            return selectedDate < currentDate;
+        }
+
+        // Listen for changes in date input
+        document.getElementById('selectedDate').addEventListener('change', function() {
+            renderTable(originalData);
+        });
