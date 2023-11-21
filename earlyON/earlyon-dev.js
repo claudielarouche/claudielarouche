9dd64af0facbfab02d28240ccbf029d38a474045
Claudie Larouche <claudielarouche@gmail.com>
2023-11-20 21:21:56 -0500

    date

--- earlyON/earlyon-dev.js
+++ earlyON/earlyon-dev.js
@@ -114,7 +114,11 @@
     const selectedDate = new Date(dateString);
     selectedDate.setHours(0, 0, 0, 0); // Set time to midnight
 
-    return selectedDate <= currentDate;
+	console.log('currentDate:', currentDate);
+console.log('selectedDate:', selectedDate);
+console.log(selectedDate < currentDate);
+
+    return selectedDate < currentDate;
 }
 
 
