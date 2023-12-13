9f7d3d8f6248816d18868c2a0c513b61e5de04b9
Claudie Larouche <claudielarouche@gmail.com>
2023-12-12 21:20:50 -0500

    Update earlyon-dev.js

--- earlyON/earlyon-dev.js
+++ earlyON/earlyon-dev.js
@@ -50,21 +50,10 @@ function renderTable(data) {
 	
 
     let tableHtml = '<table id="dataTable"><thead><tr>';
-    // Get the unique headers in the order they appear in the data
-	const uniqueHeaders = Array.from(new Set(originalData.flatMap(row => Object.keys(row))));
-
-	// Process the header row
-	tableHtml += '<thead><tr>';
-	uniqueHeaders.forEach(header => {
-		if (header !== 'URL') {
-			tableHtml += `<th>${header}</th>`;
-		}
-	});
-	// Manually include "Time of Day" and "Day of Week" and "URL" columns in the header
-	tableHtml += '<th>Time of Day</th>';
-	tableHtml += '<th>Day of Week</th>';
-	tableHtml += '<th>URL</th></tr></thead><tbody>';
-
+    headers.forEach(header => {
+        tableHtml += `<th>${header}</th>`;
+    });
+    tableHtml += '</tr></thead><tbody>';
 
     const selectedDate = document.getElementById('selectedDate').value;
 
@@ -81,55 +70,43 @@ function renderTable(data) {
     const currentDate = row['Date'] ? row['Date'] : '';
     if (!isPastDate(currentDate)) {
         tableHtml += '<tr>';
-
-        // Process other columns except for 'URL'
         headers.forEach(header => {
-            if (header !== 'URL') {
-                if (header === 'Day of Week') {
-                    // Calculate and display the day of the week
-                    const dateParts = currentDate.split('-');
-                    const currentJsDate = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
-                    const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][currentJsDate.getDay()];
-                    tableHtml += `<td>${dayOfWeek}</td>`;
-                } else if (header === 'Time of Day') {
-                    // Calculate and display the time of day
-                    const time = row['Hours'] ? row['Hours'].trim() : '';
-                    if (time.startsWith('09') || time.startsWith('10') || time.startsWith('11')) {
-                        tableHtml += '<td>Morning</td>';
-                    } else if (time.startsWith('12') || time.startsWith('13') || time.startsWith('14') || time.startsWith('15') || time.startsWith('16')) {
-                        tableHtml += '<td>Afternoon</td>';
-                    } else {
-                        tableHtml += '<td>Evening</td>';
-                    }
-                } else if (header === 'Location Address') {
-                    // Create a link with the Google Maps URL for the address
-                    const address = row[header] ? row[header].trim() : '';
-                    if (address !== '') {
-                        const googleMapsLink = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)},+Ottawa,+Canada`;
-                        tableHtml += `<td><a href="${googleMapsLink}" >${address}</a></td>`;
-                    } else {
-                        tableHtml += '<td></td>';
-                    }
+            if (header === 'URL') {
+                // Make the URL clickable as a link
+                tableHtml += `<td><a href="${row[header]}" target="_blank">URL</a></td>`;
+            } else if (header === 'Day of Week') {
+                // Calculate and display the day of the week
+                const dateParts = currentDate.split('-');
+                const currentJsDate = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
+                const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][currentJsDate.getDay()];
+                tableHtml += `<td>${dayOfWeek}</td>`;
+            } else if (header === 'Time of Day') {
+                // Calculate and display the time of day
+                const time = row['Hours'] ? row['Hours'].trim() : '';
+                if (time.startsWith('09') || time.startsWith('10') || time.startsWith('11')) {
+                    tableHtml += '<td>Morning</td>';
+                } else if (time.startsWith('12') || time.startsWith('13') || time.startsWith('14') || time.startsWith('15') || time.startsWith('16')) {
+                    tableHtml += '<td>Afternoon</td>';
                 } else {
-                    // Display other columns
-                    tableHtml += `<td>${row[header]}</td>`;
+                    tableHtml += '<td>Evening</td>';
                 }
+            } else if (header === 'Location Address') {
+                // Create a link with the Google Maps URL for the address
+                const address = row[header] ? row[header].trim() : '';
+                if (address !== '') {
+                    const googleMapsLink = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)},+Ottawa,+Canada`;
+                    tableHtml += `<td><a href="${googleMapsLink}" >${address}</a></td>`;
+                } else {
+                    tableHtml += '<td></td>';
+                }
+            } else {
+                // Display other columns
+                tableHtml += `<td>${row[header]}</td>`;
             }
         });
-
-        // Process the 'URL' column separately and make it the last column
-        const url = row['URL'] ? row['URL'].trim() : '';
-        if (url !== '') {
-             // Make the URL clickable as a link
-				tableHtml += `<td><a href="${url}" target="_blank">URL</a></td>`;
-        } else {
-            tableHtml += '<td></td>';
-        }
-
         tableHtml += '</tr>';
     }
 });
-
 
 	
    
@@ -163,7 +140,7 @@ function renderTable(data) {
 			]
 		});
     }
-	
+	$('#dataTable_filter input').val(currentSearchValue).trigger('input');
 }
 
 function filterDataByDate(data, selectedDate) {
@@ -195,7 +172,11 @@ function isPastDate(dateString) {
     return selectedDate < currentDate;
 }
 
+let currentSearchValue = ''; // Variable to store the current search value
+
+
 // Listen for changes in date input
 document.getElementById('selectedDate').addEventListener('change', function() {
+	currentSearchValue = $('#dataTable_filter input').val();
     renderTable(originalData);
 });
