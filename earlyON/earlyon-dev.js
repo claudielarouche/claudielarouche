5e3f015e83c4669a7fc40ba9d605d2c87b85e9a2
Claudie Larouche <claudielarouche@gmail.com>
2023-12-15 22:05:26 -0500

    add area filter

--- earlyON/earlyon-dev.js
+++ earlyON/earlyon-dev.js
@@ -1,159 +1,166 @@
 let originalData = []; // Initialize as an empty array
 
-window.onload = function () {
-    // Update the path to your CSV file
-    const csvFilePath = 'https://claudielarouche.com/earlyON/data-dev.csv';
+window.onload = function() {
+	// Update the path to your CSV file
+	const csvFilePath = 'https://claudielarouche.com/earlyON/data-dev.csv';
 
-    Papa.parse(csvFilePath, {
-        header: true,
-        download: true,
-        skipEmptyLines: true,
-        complete: function(results) {
-            if (results.errors.length > 0) {
-                console.error('Error parsing CSV:', results.errors);
-                document.getElementById('csvData').innerHTML = 'Error loading data.';
-            } else if (Array.isArray(results.data)) {
-                originalData = results.data;
-                renderTable(originalData);
-            } else {
-                console.error('Error loading data: Data is not an array.');
-                document.getElementById('csvData').innerHTML = 'Error loading data.';
-            }
-        },
-        error: function(error) {
-            console.error('Error fetching or parsing CSV:', error);
-            document.getElementById('csvData').innerHTML = 'Error loading data.';
-        }
-    });
+	Papa.parse(csvFilePath, {
+		header: true,
+		download: true,
+		skipEmptyLines: true,
+		complete: function(results) {
+			if (results.errors.length > 0) {
+				console.error('Error parsing CSV:', results.errors);
+				document.getElementById('csvData').innerHTML = 'Error loading data.';
+			} else if (Array.isArray(results.data)) {
+				originalData = results.data;
+				renderTable(originalData);
+			} else {
+				console.error('Error loading data: Data is not an array.');
+				document.getElementById('csvData').innerHTML = 'Error loading data.';
+			}
+		},
+		error: function(error) {
+			console.error('Error fetching or parsing CSV:', error);
+			document.getElementById('csvData').innerHTML = 'Error loading data.';
+		}
+	});
 };
 
 function renderTable(data) {
-	
-	
-    // Ensure data is an array
-    if (!Array.isArray(data)) {
-        console.error('Error loading data: Data is not an array.');
-        document.getElementById('csvData').innerHTML = 'Error loading data.';
-        return;
-    }
 
-    // Check if data is empty
-    if (data.length === 0) {
-        console.warn('No data available.');
-        document.getElementById('csvData').innerHTML = 'No data available.';
-        return;
-    }
 
-    const headers = Object.keys(data[0]);
-/*	headers.push('Time of Day'); // Add this line to include the new header
-	headers.push('Day of Week'); // Add this line to include the new header*/
-	
+	// Ensure data is an array
+	if (!Array.isArray(data)) {
+		console.error('Error loading data: Data is not an array.');
+		document.getElementById('csvData').innerHTML = 'Error loading data.';
+		return;
+	}
 
-    let tableHtml = '<table id="dataTable"><thead><tr>';
-    headers.forEach(header => {
-        tableHtml += `<th>${header}</th>`;
-    });
-    tableHtml += '</tr></thead><tbody>';
+	// Check if data is empty
+	if (data.length === 0) {
+		console.warn('No data available.');
+		document.getElementById('csvData').innerHTML = 'No data available.';
+		return;
+	}
 
-    const selectedDate = document.getElementById('selectedDate').value;
+	const headers = Object.keys(data[0]);
+	/*	headers.push('Time of Day'); // Add this line to include the new header
+		headers.push('Day of Week'); // Add this line to include the new header*/
 
-    const filteredData = filterDataByDate(data, selectedDate);
 
-    if (!Array.isArray(filteredData)) {
-        console.error('Error loading data: Filtered data is not an array.');
-        document.getElementById('csvData').innerHTML = 'Error loading data.';
-        return;
-    }
-	
-	    //let totalData = 0;
-    filteredData.forEach(row => {
-    const currentDate = row['Date'] ? row['Date'] : '';
-    if (!isPastDate(currentDate)) {
-        tableHtml += '<tr>';
-        headers.forEach(header => {
-            if (header === 'URL') {
-                // Make the URL clickable as a link
-                tableHtml += `<td><a href="${row[header]}" target="_blank">URL</a></td>`;
-            } /*else if (header === 'Day of Week') {
-                // Calculate and display the day of the week
-                const dateParts = currentDate.split('-');
-                const currentJsDate = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
-                const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][currentJsDate.getDay()];
-                tableHtml += `<td>${dayOfWeek}</td>`;
-            } else if (header === 'Time of Day') {
-                // Calculate and display the time of day
-                const time = row['Hours'] ? row['Hours'].trim() : '';
-                if (time.startsWith('09') || time.startsWith('10') || time.startsWith('11')) {
-                    tableHtml += '<td>Morning</td>';
-                } else if (time.startsWith('12') || time.startsWith('13') || time.startsWith('14') || time.startsWith('15') || time.startsWith('16')) {
-                    tableHtml += '<td>Afternoon</td>';
-                } else {
-                    tableHtml += '<td>Evening</td>';
-                }
-            } */else if (header === 'Location Address') {
-                // Create a link with the Google Maps URL for the address
-                const address = row[header] ? row[header].trim() : '';
-                if (address !== '') {
-                    const googleMapsLink = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)},+Ottawa,+Canada`;
-                    tableHtml += `<td><a href="${googleMapsLink}" >${address}</a></td>`;
-                } else {
-                    tableHtml += '<td></td>';
-                }
-            } else {
-                // Display other columns
-                tableHtml += `<td>${row[header]}</td>`;
-            }
-        });
-        tableHtml += '</tr>';
-    }
-});
+	let tableHtml = '<table id="dataTable"><thead><tr>';
+	headers.forEach(header => {
+		tableHtml += `<th>${header}</th>`;
+	});
+	tableHtml += '</tr></thead><tbody>';
 
-	
-   
+	const selectedArea = document.getElementById('selectedArea').value;
+	const selectedDate = document.getElementById('selectedDate').value;
+	const filteredData = filterDataByDateAndArea(data, selectedDate, selectedArea);
 
-    
+	//const filteredData = filterDataByDate(data, selectedDate);
 
-    tableHtml += '</tbody></table>';
+	if (!Array.isArray(filteredData)) {
+		console.error('Error loading data: Filtered data is not an array.');
+		document.getElementById('csvData').innerHTML = 'Error loading data.';
+		return;
+	}
 
-    document.getElementById('csvData').innerHTML = tableHtml;
-    
-    /*if (totalData > 0) {
-        document.getElementById('csvData').innerHTML = 'No playgroups found for the selected day.';
-        return;
-    }*/
+	//let totalData = 0;
+	filteredData.forEach(row => {
+		const currentDate = row['Date'] ? row['Date'] : '';
+		if (!isPastDate(currentDate)) {
+			tableHtml += '<tr>';
+			headers.forEach(header => {
+				if (header === 'URL') {
+					// Make the URL clickable as a link
+					tableHtml += `<td><a href="${row[header]}" target="_blank">URL</a></td>`;
+				}
+				/*else if (header === 'Day of Week') {
+				               // Calculate and display the day of the week
+				               const dateParts = currentDate.split('-');
+				               const currentJsDate = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
+				               const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][currentJsDate.getDay()];
+				               tableHtml += `<td>${dayOfWeek}</td>`;
+				           } else if (header === 'Time of Day') {
+				               // Calculate and display the time of day
+				               const time = row['Hours'] ? row['Hours'].trim() : '';
+				               if (time.startsWith('09') || time.startsWith('10') || time.startsWith('11')) {
+				                   tableHtml += '<td>Morning</td>';
+				               } else if (time.startsWith('12') || time.startsWith('13') || time.startsWith('14') || time.startsWith('15') || time.startsWith('16')) {
+				                   tableHtml += '<td>Afternoon</td>';
+				               } else {
+				                   tableHtml += '<td>Evening</td>';
+				               }
+				           } */
+				else if (header === 'Location Address') {
+					// Create a link with the Google Maps URL for the address
+					const address = row[header] ? row[header].trim() : '';
+					if (address !== '') {
+						const googleMapsLink = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)},+Ottawa,+Canada`;
+						tableHtml += `<td><a href="${googleMapsLink}" >${address}</a></td>`;
+					} else {
+						tableHtml += '<td></td>';
+					}
+				} else {
+					// Display other columns
+					tableHtml += `<td>${row[header]}</td>`;
+				}
+			});
+			tableHtml += '</tr>';
+		}
+	});
 
-    // Initialize DataTable only once
-   /* if (!$.fn.dataTable.isDataTable('#dataTable')) {
+
+
+
+	tableHtml += '</tbody></table>';
+
+	document.getElementById('csvData').innerHTML = tableHtml;
+
+	/*if (totalData > 0) {
+	    document.getElementById('csvData').innerHTML = 'No playgroups found for the selected day.';
+	    return;
+	}*/
+
+	// Initialize DataTable only once
+	/* if (!$.fn.dataTable.isDataTable('#dataTable')) {
         $('#dataTable').DataTable({
 			"pageLength": -1,
 			"lengthChange": false, // Hide the "Show x entries" dropdown
 			"searching": false
 		});
     }*/
-	
+
 	if (!$.fn.dataTable.isDataTable('#dataTable')) {
-        $('#dataTable').DataTable({
+		$('#dataTable').DataTable({
 			"pageLength": -1,
 			"dom": 'Bfrtip', // 'B' for buttons
 			"buttons": [
 				'colvis' // Column visibility button
 			]
 		});
-    }
+	}
 	$('#dataTable_filter input').val(currentSearchValue).trigger('input');
 }
 
-function filterDataByDate(data, selectedDate) {
-    // If no date is selected, return the original data
-    if (!selectedDate) {
+function filterDataByDateAndArea(data, selectedDate, selectedArea) {
+    // If no date or area is selected, return the original data
+    if (!selectedDate && !selectedArea) {
         return data;
     }
-
-    // Ensure data is an array before filtering
+	
+	 // Ensure data is an array before filtering
     if (Array.isArray(data)) {
         return data.filter(row => {
             const currentDate = row['Date'] ? row['Date'] : '';
-            return currentDate === selectedDate;
+            const currentArea = row['Area'] ? row['Area'] : '';
+
+            const dateCondition = !selectedDate || currentDate === selectedDate;
+            const areaCondition = !selectedArea || currentArea === selectedArea;
+
+            return dateCondition && areaCondition;
         });
     } else {
         return [];
@@ -161,15 +168,15 @@ function filterDataByDate(data, selectedDate) {
 }
 
 function isPastDate(dateString) {
-    const currentDate = new Date();
-    currentDate.setHours(0, 0, 0, 0); // Set time to midnight
+	const currentDate = new Date();
+	currentDate.setHours(0, 0, 0, 0); // Set time to midnight
 
-    // Parse the date string in "YYYY-MM-DD" format
-    const dateParts = dateString.split('-');
-    const selectedDate = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
-    selectedDate.setHours(0, 0, 0, 0); // Set time to midnight
+	// Parse the date string in "YYYY-MM-DD" format
+	const dateParts = dateString.split('-');
+	const selectedDate = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
+	selectedDate.setHours(0, 0, 0, 0); // Set time to midnight
 
-    return selectedDate < currentDate;
+	return selectedDate < currentDate;
 }
 
 let currentSearchValue = ''; // Variable to store the current search value
@@ -178,5 +185,10 @@ let currentSearchValue = ''; // Variable to store the current search value
 // Listen for changes in date input
 document.getElementById('selectedDate').addEventListener('change', function() {
 	currentSearchValue = $('#dataTable_filter input').val();
+	renderTable(originalData);
+});
+
+// Listen for changes in the Area select input
+document.getElementById('selectedArea').addEventListener('change', function() {
     renderTable(originalData);
 });
