81fd654d75c30dd17214becf1949c1a9332d925d
Claudie Larouche <claudielarouche@gmail.com>
2024-04-17 21:53:15 -0400

    Update earlyon.js

--- earlyON/earlyon.js
+++ earlyON/earlyon.js
@@ -1,6 +1,9 @@
-console.log('merge URL 2');
+//console.log('baby scale 28, ready to be uploaded to prod!');
 
 let originalData = []; // Initialize as an empty array
+let babyScaleVisible = false; // Flag to track if the Baby Scale column is visible
+let timeOfDayVisible = false; // Flag to track if the Time of Day column is visible
+let dayOfWeekVisible = false; // Flag to track if the Day of Week column is visible
 
 function getQueryParam(key) {
     const params = new URLSearchParams(window.location.search);
@@ -9,7 +12,7 @@ function getQueryParam(key) {
 
 window.onload = function() {
 	// Update the path to your CSV file
-	const csvFilePath = 'https://claudielarouche.com/earlyON/data.csv';
+	const csvFilePath = 'https://claudielarouche.com/earlyON/data-dev.csv';
 
 	Papa.parse(csvFilePath, {
 		header: true,
@@ -61,23 +64,23 @@ function renderTable(data) {
 	tableHtml += '<th>Actions</th></tr></thead><tbody>';
 	
 
-	//const selectedArea = document.getElementById('selectedArea').value;
 	const selectedDate = document.getElementById('selectedDate').value;
 	const selectedAgeGroup = document.getElementById('selectedAgeGroup').value;
-	
-	const selectedScheduleCheckboxes = document.querySelectorAll('.scheduleCheckbox');
-    const selectedSchedule = Array.from(selectedScheduleCheckboxes)
-        .filter(checkbox => checkbox.checked)
-        .map(checkbox => checkbox.id.replace('Checkbox', ''));
+	const selectedSchedule = document.getElementById('scheduleFilter').value;
+	const babyScaleCheckbox = document.getElementById('babyScaleCheckbox');
 
-    //const filteredData = filterData(data, selectedDate, selectedArea, selectedAgeGroup);
-	const filteredData = filterData(data, selectedDate, selectedAgeGroup);
+	
+	const filteredData = filterData(data, selectedDate, selectedAgeGroup, selectedLanguages, selectedAreas, selectedSchedule, babyScaleCheckbox);
 
 	if (!Array.isArray(filteredData)) {
 		console.error('Error loading data: Filtered data is not an array.');
 		document.getElementById('csvData').innerHTML = 'Error loading data.';
 		return;
 	}
+
+	let babyScaleIndex; // Declare babyScaleIndex variable
+	let timeOfDayIndex;
+	let dayOfWeekIndex;
 
 	//let totalData = 0;
 	filteredData.forEach(row => {
@@ -88,15 +91,30 @@ function renderTable(data) {
 
 			// Start building the row with a conditional background color
 			tableHtml += `<tr${isCancelled ? ' style="background-color: #FFCCCB;"' : ''}>`;
+			
 
-			headers.forEach(header => {
+			headers.forEach((header, index) => {
 			    switch(header) {
+				case 'Baby Scale': 	
+				    // Assign the index of the "Baby Scale" column to the babyScaleIndex variable
+                    		    babyScaleIndex = index;					  
+				    tableHtml += `<td>${row[header]}</td>`;
+				    break;
+				case 'Time of Day': 	
+				    //-1 because Time of Day is after URL
+                    		    timeOfDayIndex = index - 1;					  
+				    tableHtml += `<td>${row[header]}</td>`;
+				    break;
+				case 'Day of Week': 	
+				    //-1 because Day of Week is after URL
+                    		    dayOfWeekIndex = index - 1;					  
+				    tableHtml += `<td>${row[header]}</td>`;
+				    break;
 			        case 'URL':
-			            // Make the URL clickable as a link
-			            // tableHtml += `<td><a href="${row[header]}" target="_blank">URL</a></td>`;
+				    //Skip the URL column
 			            break;
 				 case 'Playgroup Name':
-				    // Merge URL with School Name to create a clickable link
+				    // Merge URL with Name to create a clickable link
 		                        const url = row['URL'] ? row['URL'] : '';
 		                        const playgroupName = row[header] ? row[header] : '';
 		                        if (url !== '' && playgroupName !== '') {
@@ -135,6 +153,7 @@ function renderTable(data) {
 
 	document.getElementById('csvData').innerHTML = tableHtml;
 
+	
 
 	if (!$.fn.dataTable.isDataTable('#dataTable')) {
 		$('#dataTable').DataTable({
@@ -143,6 +162,20 @@ function renderTable(data) {
 			"buttons": [
 				'colvis' // Column visibility button
 			],
+			 "columnDefs": [
+			    {
+				"targets": babyScaleIndex, // Index of the Baby Scale column
+				"visible": babyScaleVisible // Make the Baby Scale column hidden initially
+			    },
+			    {
+				"targets": timeOfDayIndex, // Index of the Time of Day column
+				"visible": timeOfDayVisible // Make the Time of Day column hidden initially
+			    },
+			    {
+				"targets": dayOfWeekIndex, // Index of the Day of Week column
+				"visible": dayOfWeekVisible // Make the Day of Week column hidden initially
+			    }
+			],
 			"language": {
 				"emptyTable": "No data available in table, try <a href='javascript:void(0);' onclick='clearAllFilters()'>resetting all filters to default</a>.",
 				"zeroRecords": "No data available in table, try <a href='javascript:void(0);' onclick='clearAllFilters()'>resetting all filters to default</a>."
@@ -150,77 +183,74 @@ function renderTable(data) {
 		});
 	}
 	$('#dataTable_filter input').val(currentSearchValue).trigger('input');
+
+	// Listen for column visibility event
+	$('#dataTable').on('column-visibility.dt', function (e, settings, column, state) {
+		console.log("listening");
+	    if (column === babyScaleIndex) {
+		    console.log("scale");
+		babyScaleVisible = state; // Update the flag based on the visibility state of the Baby Scale column
+		    console.log(babyScaleVisible);
+	    } else if (column === timeOfDayIndex) {
+		    console.log("time");
+		timeOfDayVisible = state; // Update the flag based on the visibility state of the Time of Day column
+		    console.log(timeOfDayVisible);
+	    } else if (column === dayOfWeekIndex) {
+		    console.log("day");
+		dayOfWeekVisible = state; // Update the flag based on the visibility state of the Day of Week column
+		    console.log(dayOfWeekVisible);
+	    }
+	});
+	
 }
 
 
-function filterData(data, selectedDate, selectedAgeGroup) {
-    const scheduleFilter = document.getElementById('scheduleFilter').value;
-	
-	 if (selectedLanguages.length === 0) {
-        return [];
-    }
-	
-    // If no date, area, age group, or schedule is selected, return the original data
-    if (!selectedDate && !selectedAreas.length && !selectedAgeGroup && scheduleFilter === 'all'  && !selectedLanguages.length) {
+function filterData(data, selectedDate, selectedAgeGroup, selectedLanguages, selectedAreas, scheduleFilter, babyScaleFilter) {
+    // If no date, age group, languages, areas, or schedule is selected, return the original data
+    if (!selectedDate && !selectedAgeGroup && selectedLanguages.length === 0 && selectedAreas.length === 0 && scheduleFilter === 'all') {
         return data;
     }
 
-    // Ensure data is an array before filtering
-    if (Array.isArray(data)) {
-        return data.filter(row => {
-            const currentDate = row['Date'] ? row['Date'] : '';
-            //const currentArea = row['Area'] ? row['Area'] : '';
-            const currentAgeGroup = row['Age Group'] ? row['Age Group'] : '';
-            const currentTimeOfDay = row['Time of Day'] ? row['Time of Day'] : '';
-            const currentDayOfWeek = row['Day of Week'] ? row['Day of Week'] : '';
-			const currentLanguage = row['Language'] ? row['Language'] : ''; 
-
-            const dateCondition = !selectedDate || currentDate === selectedDate;
-            //const areaCondition = !selectedArea || currentArea === selectedArea;
-            const ageGroupCondition = !selectedAgeGroup || currentAgeGroup.includes(selectedAgeGroup);			
-			const languageCondition = !selectedLanguages.length || selectedLanguages.some(lang => row['Language'].toLowerCase().includes(lang.toLowerCase())); //Not exact match
-			const areaCondition = !selectedAreas.length || selectedAreas.some(lang => row['Area'].toLowerCase().includes(lang.toLowerCase())); //Not exact match
-
-            switch (scheduleFilter) {
-                case 'all':
-                    // Show all rows
-                    return dateCondition && areaCondition && ageGroupCondition && languageCondition && areaCondition;
-
-                case 'eveningsWeekends':
-                    // Show evenings and weekends only
-                    return (
-                        dateCondition &&
-                        areaCondition &&
-						languageCondition &&
-                        ageGroupCondition &&
-                        (currentTimeOfDay === 'Evening' ||
-                            currentDayOfWeek === 'Saturday' ||
-                            currentDayOfWeek === 'Sunday')
-                    );
-
-                case 'weekdayAMPM':
-				// Show weekday AM and PM only
-				return (
-					dateCondition &&
-					areaCondition &&
-					languageCondition &&
-					ageGroupCondition &&
-					(
-						(currentDayOfWeek !== 'Saturday' && currentDayOfWeek !== 'Sunday') &&
-						(currentTimeOfDay === 'Morning' || currentTimeOfDay === 'Afternoon')
-					)
-				);
+    return data.filter(row => {
+        const currentDate = row['Date'] || '';
+        const currentAgeGroup = row['Age Group'] || '';
+        const currentTimeOfDay = row['Time of Day'] || '';
+        const currentDayOfWeek = row['Day of Week'] || '';
+        const currentLanguage = row['Language'] || '';
+        const currentArea = row['Area'] || '';
+	const currentBabyScale = row['Baby Scale'] || ''; 
+	   
 
 
-                default:
-                    // Handle other cases
-                    return false;
-            }
-        });
-    } else {
-        return [];
-    }
+        const dateCondition = !selectedDate || currentDate === selectedDate;
+        const ageGroupCondition = !selectedAgeGroup || currentAgeGroup.includes(selectedAgeGroup);
+        const languageCondition = !selectedLanguages.length || selectedLanguages.some(lang => currentLanguage.toLowerCase().includes(lang.toLowerCase()));
+        const areaCondition = !selectedAreas.length || selectedAreas.some(area => currentArea.toLowerCase().includes(area.toLowerCase()));
+	// If babyScaleCheckbox is checked, include only rows where 'Baby Scale' is 'Yes'
+    	const babyScaleCondition = !babyScaleFilter.checked || currentBabyScale === 'Yes';
+
+        let scheduleFilterCondition = true;
+
+	   
+        switch (scheduleFilter) {
+            case 'all':
+                scheduleFilterCondition = true;
+			
+                break;
+            case 'eveningsWeekends':
+                scheduleFilterCondition = currentTimeOfDay === 'Evening' || currentDayOfWeek === 'Saturday' || currentDayOfWeek === 'Sunday';
+			
+                break;
+            case 'weekdayAMPM':
+			
+                scheduleFilterCondition = (currentDayOfWeek !== 'Saturday' && currentDayOfWeek !== 'Sunday') && (currentTimeOfDay === 'Morning' || currentTimeOfDay === 'Afternoon');
+                break;
+        }
+
+        return dateCondition && ageGroupCondition && languageCondition && areaCondition && scheduleFilterCondition && babyScaleCondition;
+    });
 }
+
 
 
 function isPastDate(dateString) {
@@ -343,6 +373,15 @@ document.querySelectorAll('.areaCheckbox').forEach(function (checkbox) {
     }
 });
 
+
+// Listen for changes in the baby scale checkbox 
+document.getElementById('babyScaleCheckbox').addEventListener('change', function() {	
+	currentSearchValue = $('#dataTable_filter input').val();
+    renderTable(originalData);
+	console.log("babyscalecheckbox changed")
+});
+
+
 function clearAllFilters() {
     // Clear the date filter
     document.getElementById('selectedDate').value = '';
@@ -379,5 +418,3 @@ function clearAllFilters() {
     // Render the table with cleared filters
     renderTable(originalData);
 }
-
-
