ee896de505cd0b1ff76f03fcad764ea8e1e832eb
Claudie Larouche <claudielarouche@gmail.com>
2024-04-18 21:38:55 -0400

    Update ottawa-dev.js

--- ottawa/ottawa-dev.js
+++ ottawa/ottawa-dev.js
@@ -92,6 +92,15 @@ function renderTable(data) {
                             tableHtml += '<td></td>';
                         }
                         break;
+		    case 'Reservation':
+	                // Handle Facility URL content
+	                const facilityUrl = row[header] ? row[header] : '';
+	                if (facilityUrl === '#') {
+	                    tableHtml += '<td>N/A</td>';
+	                } else {
+	                    tableHtml += `<td><a href="${facilityUrl}" target="_blank">Reservation</a></td>`;
+	                }
+	                break;
 
                     default:
                         // Display other columns
