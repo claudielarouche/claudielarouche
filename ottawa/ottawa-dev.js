3379dc1efb04a183e69d3435446e638b58c6ab61
Claudie Larouche <claudielarouche@gmail.com>
2024-04-20 21:41:28 -0400

    Update ottawa-dev.js

--- ottawa/ottawa-dev.js
+++ ottawa/ottawa-dev.js
@@ -1,4 +1,4 @@
-console.log('Add filter v6');
+console.log('Add filter v7');
 
 let originalData = []; // Initialize as an empty array
 
@@ -178,11 +178,7 @@ document.getElementById('showToday').addEventListener('click', function(event) {
 });
 
 function filterData(data, selectedAreas) {
-    // If no date, age group, languages, areas, or schedule is selected, return the original data
-    //if (!selectedDate && !selectedAgeGroup && selectedLanguages.length === 0 && selectedAreas.length === 0 && scheduleFilter === 'all') {
-    if (selectedAreas.length === 0) {
-        return data;
-    }
+
 
     return data.filter(row => {
         /*const currentDate = row['Date'] || '';
