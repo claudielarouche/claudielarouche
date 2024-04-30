d3733e4c4f7db4bf4f140a03bf80e5bdcdcf2dd1
Claudie Larouche <claudielarouche@gmail.com>
2024-04-29 21:42:40 -0400

    Update ottawa-dev.js

--- ottawa/ottawa-dev.js
+++ ottawa/ottawa-dev.js
@@ -10,7 +10,7 @@ function getQueryParam(key) {
 
 window.onload = function() {
 	// Update the path to your CSV file
-	const csvFilePath = 'https://claudielarouche.com/ottawa/data.csv';
+	const csvFilePath = 'https://claudielarouche.com/ottawa/data-dev.csv';
 
 	Papa.parse(csvFilePath, {
 		header: true,
