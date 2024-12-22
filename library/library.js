faad373c6e5a950249cbb6fe131d60ca78fd9cba
Claudie Larouche <claudielarouche@gmail.com>
2024-12-22 11:44:25 -0500

    Update library.js

--- library/library.js
+++ library/library.js
@@ -10,7 +10,7 @@ function getQueryParam(key) {
 
 window.onload = function() {
 	// Update the path to your CSV file
-	const csvFilePath = 'https://claudielarouche.com/library/ottawa_library_programs-dev.csv';
+	const csvFilePath = 'https://claudielarouche.com/library/ottawa_library_programs.csv';
 
 	Papa.parse(csvFilePath, {
 		header: true,
