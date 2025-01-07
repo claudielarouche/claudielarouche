a4cc971e68139805ed1be63f2de3bcd16209d407
Claudie Larouche <claudielarouche@gmail.com>
2025-01-07 16:22:45 -0500

    Update skate.js

--- skate/skate.js
+++ skate/skate.js
@@ -1,5 +1,7 @@
-console.log('version 2');
 
+console.log('remove category v4');
+
+let sortingState;
 let originalData = []; // Initialize as an empty array
 
 function getQueryParam(key) {
@@ -9,7 +11,7 @@ function getQueryParam(key) {
 
 window.onload = function() {
 	// Update the path to your CSV file
-	const csvFilePath = 'https://claudielarouche.com/skate/data.csv';
+	const csvFilePath = 'https://claudielarouche.com/ottawa/data.csv';
 
 	Papa.parse(csvFilePath, {
 		header: true,
@@ -42,6 +44,8 @@ function renderTable(data) {
         return;
     }
 
+   let sortOrderIndex; 
+
     // Check if data is empty
     if (data.length === 0) {
         console.warn('No data available.');
@@ -54,24 +58,32 @@ function renderTable(data) {
     let tableHtml = '<table id="dataTable"><thead><tr>';
     headers.forEach(header => {
         // Skip rendering the URL column
-        if (header !== 'Sort Order' && header !== 'URL') {
+        if (header !== 'URL' && header !== 'Category') {
             tableHtml += `<th>${header}</th>`;
         }
     });
     tableHtml += '</tr></thead><tbody>';
 
+    const filteredData = filterData(data, selectedAreas, selectedDay, selectedAge, selectedTime);
+
     // Iterate through each row of data
-    data.forEach(row => {
+    filteredData.forEach(row => {
         const currentDate = row['Date'] ? row['Date'] : '';
 
         // Start building the row with a conditional background color
         tableHtml += '<tr>';
 
-        headers.forEach(header => {
+        headers.forEach((header, index) => {
             // Skip rendering the URL column
-            if (header !== 'Sort Order' && header !== 'URL') {
+            if (header !== 'URL' && header !== 'Category') {
                 switch (header) {
-                    case 'Pool Name':
+		    case 'Sort Order': 	
+				    // Assign the index of the "Baby Scale" column to the babyScaleIndex variable
+				// minus 1 because it is after the URL column which is not shown
+                    		    sortOrderIndex = index;					  
+				    tableHtml += `<td>${row[header]}</td>`;
+				    break;
+                    case 'Facility Name':
                         // Merge URL with Pool Name to create a clickable link
                         const url = row['URL'] ? row['URL'] : '';
                         const poolName = row[header] ? row[header] : '';
@@ -92,6 +104,16 @@ function renderTable(data) {
                             tableHtml += '<td></td>';
                         }
                         break;
+
+		case 'Reservation':
+                // Handle Facility URL content
+                const facilityUrl = row[header] ? row[header] : '';
+                if (facilityUrl === '#') {
+                    tableHtml += '<td>N/A</td>';
+                } else {
+                    tableHtml += `<td><a href="${facilityUrl}" target="_blank">Facility Reservation Page</a></td>`;
+                }
+                break;
 
                     default:
                         // Display other columns
@@ -115,6 +137,15 @@ function renderTable(data) {
             "buttons": [
                 'colvis' // Column visibility button
             ],
+
+	    "columnDefs": [
+			    {
+				"targets": sortOrderIndex, 
+				"visible": false 
+			    }
+			  
+			],
+		"order": [[0, 'asc'], [2, 'asc'], [3, 'asc']],
             "language": {
                 "emptyTable": "No data available in table, try <a href='javascript:void(0);' onclick='clearAllFilters()'>resetting all filters to default</a>.",
                 "zeroRecords": "No data available in table, try <a href='javascript:void(0);' onclick='clearAllFilters()'>resetting all filters to default</a>."
@@ -122,6 +153,11 @@ function renderTable(data) {
         });
     }
     $('#dataTable_filter input').val(currentSearchValue).trigger('input');
+
+    //If sortingState is set, sort the table by sortingState
+    if (sortingState) {
+        $('#dataTable').DataTable().order(sortingState.order).draw();
+    }
 }
 
 document.getElementById('showToday').addEventListener('click', function(event) {
@@ -137,18 +173,77 @@ document.getElementById('showToday').addEventListener('click', function(event) {
 
     currentSearchValue = $('#dataTable_filter input').val();
 
-    currentSearchValue = todayDayOfWeek + (currentSearchValue ? " " + currentSearchValue : "");
+    if (!currentSearchValue.includes(todayDayOfWeek)) {
 
-    // Populate the search box with today's day of the week
-    document.getElementById('dataTable_filter').querySelector('input').value = currentSearchValue;
+       currentSearchValue = todayDayOfWeek + (currentSearchValue ? " " + currentSearchValue : "");
+
+       // Populate the search box with today's day of the week
+       document.getElementById('dataTable_filter').querySelector('input').value = currentSearchValue;
     
-    // Trigger the input event to initiate the search
-    document.getElementById('dataTable_filter').querySelector('input').dispatchEvent(new Event('input'));
+       // Trigger the input event to initiate the search
+       document.getElementById('dataTable_filter').querySelector('input').dispatchEvent(new Event('input'));
+    }
 });
+
+function filterData(data, selectedAreas, selectedDay, selectedAge, selectedTime) {
+
+
+    return data.filter(row => {
+
+	const currentArea = row['Area'] || '';
+	const currentCategory = row['Category'] || '';
+	const currentDay = row['Day'] || '';
+	const currentAge = row['Age'] || '';
+	const currentTime = row['Time of day'] || '';
+
+	const areaCondition = selectedAreas.some(area => currentArea.toLowerCase().includes(area.toLowerCase()));
+	const dayCondition = selectedDay.some(day => currentDay.toLowerCase() === day.toLowerCase());
+	const ageCondition = selectedAge.some(age => currentAge.toLowerCase().includes(age.toLowerCase()));
+	const timeCondition = selectedTime.some(time => currentTime.toLowerCase().includes(time.toLowerCase()));
+	const skateCondition = currentCategory === "Skating";
+	    
+	return areaCondition && dayCondition && ageCondition && timeCondition && skateCondition;
+    });
+}
+
 
 let currentSearchValue = getQueryParam('search'); // Variable to store the current search value
 
 function clearAllFilters() {
+    // Store the current sorting state
+    sortingState = $('#dataTable').DataTable().state();
+	
+    // Check all the "Select Area" checkboxes
+    document.querySelectorAll('.areaCheckbox').forEach(checkbox => {
+        checkbox.checked = true;
+		if (!selectedAreas.includes(checkbox.value)) {
+			selectedAreas.push(checkbox.value);
+		}
+    });
+
+// Check all the "Select Day" checkboxes
+    document.querySelectorAll('.dayCheckbox').forEach(checkbox => {
+        checkbox.checked = true;
+		if (!selectedDay.includes(checkbox.value)) {
+			selectedDay.push(checkbox.value);
+		}
+    });
+
+// Check all the "Select Age" checkboxes
+    document.querySelectorAll('.ageCheckbox').forEach(checkbox => {
+        checkbox.checked = true;
+		if (!selectedAge.includes(checkbox.value)) {
+			selectedAge.push(checkbox.value);
+		}
+    });
+
+// Check all the "Select Time of day" checkboxes
+    document.querySelectorAll('.timeCheckbox').forEach(checkbox => {
+        checkbox.checked = true;
+		if (!selectedTime.includes(checkbox.value)) {
+			selectedTime.push(checkbox.value);
+		}
+    });
 
 
     // Clear the DataTable search box
@@ -159,3 +254,109 @@ function clearAllFilters() {
     // Render the table with cleared filters
     renderTable(originalData);
 }
+
+const selectedAreas = [];
+document.querySelectorAll('.areaCheckbox').forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+	// Store the current sorting state
+        sortingState = $('#dataTable').DataTable().state();
+        currentSearchValue = $('#dataTable_filter input').val();
+		if (checkbox.checked) {
+            if (!selectedAreas.includes(checkbox.value)) {
+                selectedAreas.push(checkbox.value);
+            }
+        } else {
+            const index = selectedAreas.indexOf(checkbox.value);
+            if (index !== -1) {
+                selectedAreas.splice(index, 1);
+            }
+        }
+        renderTable(originalData);
+    });
+
+    // Initialize with all checkboxes checked by default
+    checkbox.checked = true;
+    if (!selectedAreas.includes(checkbox.value)) {
+        selectedAreas.push(checkbox.value);
+    }
+});
+
+
+
+const selectedDay = [];
+document.querySelectorAll('.dayCheckbox').forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+	// Store the current sorting state
+        sortingState = $('#dataTable').DataTable().state();
+        currentSearchValue = $('#dataTable_filter input').val();
+		if (checkbox.checked) {
+            if (!selectedDay.includes(checkbox.value)) {
+                selectedDay.push(checkbox.value);
+            }
+        } else {
+            const index = selectedDay.indexOf(checkbox.value);
+            if (index !== -1) {
+                selectedDay.splice(index, 1);
+            }
+        }
+        renderTable(originalData);
+    });
+
+    // Initialize with all checkboxes checked by default
+    checkbox.checked = true;
+    if (!selectedDay.includes(checkbox.value)) {
+        selectedDay.push(checkbox.value);
+    }
+});
+
+const selectedAge = [];
+document.querySelectorAll('.ageCheckbox').forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+	// Store the current sorting state
+        sortingState = $('#dataTable').DataTable().state();
+        currentSearchValue = $('#dataTable_filter input').val();
+		if (checkbox.checked) {
+            if (!selectedAge.includes(checkbox.value)) {
+                selectedAge.push(checkbox.value);
+            }
+        } else {
+            const index = selectedAge.indexOf(checkbox.value);
+            if (index !== -1) {
+                selectedAge.splice(index, 1);
+            }
+        }
+        renderTable(originalData);
+    });
+
+    // Initialize with all checkboxes checked by default
+    checkbox.checked = true;
+    if (!selectedAge.includes(checkbox.value)) {
+        selectedAge.push(checkbox.value);
+    }
+});
+
+const selectedTime = [];
+document.querySelectorAll('.timeCheckbox').forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+	// Store the current sorting state
+        sortingState = $('#dataTable').DataTable().state();
+        currentSearchValue = $('#dataTable_filter input').val();
+		if (checkbox.checked) {
+            if (!selectedTime.includes(checkbox.value)) {
+                selectedTime.push(checkbox.value);
+            }
+        } else {
+            const index = selectedTime.indexOf(checkbox.value);
+            if (index !== -1) {
+                selectedTime.splice(index, 1);
+            }
+        }
+        renderTable(originalData);
+    });
+
+    // Initialize with all checkboxes checked by default
+    checkbox.checked = true;
+    if (!selectedTime.includes(checkbox.value)) {
+        selectedTime.push(checkbox.value);
+    }
+});
