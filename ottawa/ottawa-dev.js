5b39c3f53e59637584469d753099948e34b52ecc
Claudie Larouche <claudielarouche@gmail.com>
2025-02-09 14:23:29 -0500

    Update ottawa-dev.js

--- ottawa/ottawa-dev.js
+++ ottawa/ottawa-dev.js
@@ -1,6 +1,6 @@
 
 
-console.log('time of day');
+console.log('report a problem v1');
 
 let sortingState;
 let originalData = []; // Initialize as an empty array
@@ -123,6 +123,8 @@ function renderTable(data) {
                 }
             }
         });
+
+	tableHtml += `<td><a href="https://docs.google.com/forms/d/e/1FAIpQLScAtAvU5WfcL2Jkk3trRwLDq4j_dW0nzcJflcHrtdbyzOaQ3w/viewform?usp=sf_link&entry.980923575=${encodeURIComponent(row['Day'])}" target="_blank">Report a data issue</a></td>`;
 
         tableHtml += '</tr>';
     });
