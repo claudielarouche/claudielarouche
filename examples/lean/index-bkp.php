13b3fbaa47448a6bcd61837a70ef35f84c63e9c6
Claudie Larouche <claudielarouche@gmail.com>
2025-03-31 19:22:49 -0400

    Add files via upload

---
+++ examples/lean/index-bkp.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Lean Systems | Ottawa, ON K1T 0J2</title>
+
+		<!-- Bootstrap -->
+		<link href="_/css/final.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<ul class="nav nav-pills nav-justified main-menu">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About Roxy</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Cyber Training</a></li>
+			<li><a href="#">Fitness & Nutrition Questionnaire</a></li>
+		</ul>
+		
+		<div id="myCarousel" class="carousel hidden-xs slide" data-ride="carousel" data-interval="3000">
+		<!--Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
+		
+		<!-- Wrapper for slides -->
+		<section class="carousel-inner">
+			<div class="item active">
+				<img src="images/carousel/running_shoes.jpg" alt="Photo of Running Shoes">
+			</div>
+			<div class="item">
+				<img src="images/carousel/salad.jpg" alt="Photo of a healty salad">
+			</div>
+			<div class="item">
+				<img src="images/carousel/yoga.jpg" alt="Photo of a woman doing yoga">
+			</div>
+		</section>
+		<!-- Controls -->
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+		</a>
+	</div>
+		
+
+		<script src="_/js/bootstrap.js"></script>
+	</body>
+</html>
