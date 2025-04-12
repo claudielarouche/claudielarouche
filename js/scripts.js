b9f8824c20268b48abb7ac673027b3e5a20c1088
Claudie Larouche <claudielarouche@gmail.com>
2025-04-11 20:27:18 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 * Start Bootstrap - Business Casual v7.0.8 (https://startbootstrap.com/theme/business-casual)
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+Note from 2025-04-11: I have no idea if this script is needed...
 */
 // Highlights current date on contact page
 window.addEventListener('DOMContentLoaded', event => {
