5f230487457df2b79a053cebe1ccc41a08383595
Claudie Larouche <claudielarouche@gmail.com>
2025-06-25 23:44:05 -0400

    [skip netlify]

--- assets/js/playgrounds.js
+++ assets/js/playgrounds.js
@@ -264,7 +264,7 @@ document.querySelectorAll('.areaCheckbox').forEach(function (checkbox) {
 */
 
 function initMap() {
-    var map = L.map('map').setView([45.4215, -75.6972], 12);
+    map = L.map('map').setView([45.4215, -75.6972], 12);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
