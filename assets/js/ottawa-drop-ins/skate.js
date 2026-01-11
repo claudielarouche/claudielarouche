1358dff81494097a944577781f8ebef6d7e28152
Claudie Larouche <claudielarouche@gmail.com>
2026-01-11 05:34:27 -0500

    report a problem

--- assets/js/ottawa-drop-ins/skate.js
+++ assets/js/ottawa-drop-ins/skate.js
@@ -63,7 +63,7 @@ function renderTable(data) {
             tableHtml += `<th>${header}</th>`;
         }
     });
-    tableHtml += '</tr></thead><tbody>';
+    tableHtml += '<th>Actions</th></tr></thead><tbody>';
 
     const filteredData = filterData(data, selectedAreas, selectedDay, selectedAge, selectedTime);
 
@@ -123,6 +123,9 @@ function renderTable(data) {
                 }
             }
         });
+
+        tableHtml += `<td><a href="https://docs.google.com/forms/d/e/1FAIpQLScTQ6U_lnHo0kr5rGo3zSjYQwsGG5PZIfL5Eil8iVQU9UWTmg/viewform?usp=sf_link&entry.658764103=${encodeURIComponent('Ottawa Drop-Ins: ' + row['Day'] + ', ' + row['Time'] + ', ' + row['Facility Name'] + ', ' + row['Address'] + ', ' + row['Activity Type'] + ', ' + row['Registration Required'] + ', ' + row['Area'] + ', ' + row['Category'] + ', ' + row['Age'] + ', ' + row['Time of day'])}" target="_blank">Report a data issue</a></td>`;
+
 
         tableHtml += '</tr>';
     });
